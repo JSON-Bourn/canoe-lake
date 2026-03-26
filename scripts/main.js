cebbc16efd0f7772089f4950a1d16a6d8a1dcba2
pslempers <pslempers@gmail.com>
2026-03-26 14:21:08 -0400

    moved js logic from index.html into main.js

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// ********************************
+// *** CANOE LAKE ***
+// *** By JSON BOURN ***
+// ********************************
+
+// Main Logic File for Solve Page
+
+//This example function simply makes an element draggable...
+    //https://jqueryui.com/draggable/
+    $( function() {
+      $( ".draggable" ).draggable();
+    });
