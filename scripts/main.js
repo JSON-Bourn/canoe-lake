583519a7efa51a61241d54b172ae9bbe35686590
pslempers <pslempers@gmail.com>
2026-04-04 09:20:53 -0400

    Added GSAP's liveSnap grid for snap functionality

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
     // type: 'y',
     // bounds: document.getElementById('body'),
     inertia: true,
+    liveSnap: {
+		//snaps to the closest point in the array, but only when it's within 15px (new in GSAP 1.20.0 release):
+		points: [
+			{ x: 0, y: 0 },
+			{ x: 100, y: 0 },
+			{ x: 200, y: 50 }
+		],
+		radius: 15
+	  },
     onClick: function () {
     console.log('clicked');
     },
